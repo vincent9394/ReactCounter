535ea0da2c4fe276817e5ccb5dd7f1fdad4ab5f2
vincent <luk96515@gmail.com>
2021-11-02 02:06:41 +0800

    [react Counter][vincent] feat: add redux counterSlice.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { increment ,decrement} from "../createSlice/counterSlice";
 // import "../styles/Counter.css"
 
 function Counter(props) {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+
   const [number, setNumber] = useState(0);
     // const sum=0;
-  function increase() {
-    setNumber(number + 1);
-    props.increaseSum()
-  }
 
-  function decrease() {
-    setNumber(number - 1);
-    props.decreaseSum()
-  }
+//   function increase() {
+//     setNumber(number + 1);
+//     props.increaseSum()
+//   }
+
+//   function decrease() {
+//     setNumber(number - 1);
+//     props.decreaseSum()
+//   }
   
 
   return (
     <div>
-      <button onClick={increase}>+</button>
-      <span>{number}</span>
-      <button onClick={decrease}>-</button>
+      <button onClick={()=> {dispatch(increment())}}>+</button>
+      <span>{count}</span>
+      <button onClick={()=> {dispatch(decrement())}}>-</button>
 
     </div>
   );
--- src/components/MultiCounter.js
+++ src/components/MultiCounter.js
@@ -2,19 +2,24 @@ import CounterGroup from "./CounterGroup";
 import CounterSizeGenerator from "./CounterSizeGenerator";
 import React, { useState } from "react";
 import CounterSum from "./CounterSum";
+import { useSelector, useDispatch } from 'react-redux'
+import { increaseSum ,decreaseSum} from "../createSlice/counterSlice";
 
 function MultipleCounter() {
+    const reduxSum = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+
   const [counterSize, setCounterSize] = useState(0);
-  const [sum, setSum] = useState(0);
+//   const [sum, setSum] = useState(0);
 
   function updateCounterSize(counterSize) {
     setCounterSize(counterSize);
   }
   function increaseSum() {
-    setSum(sum + 1);
+    dispatch(increaseSum())
   }
   function decreaseSum() {
-    setSum(sum - 1);
+    dispatch(decreaseSum());
   }
 
   return (
@@ -24,7 +29,8 @@ function MultipleCounter() {
       ></CounterSizeGenerator>
       <CounterGroup counterSize={counterSize} increaseSum={increaseSum} decreaseSum={decreaseSum}></CounterGroup>
       <span>The CounterSize:{counterSize}</span>
-      <CounterSum sum={sum}></CounterSum>
+      <span>The sum is : {reduxSum} </span>
+      {/* <CounterSum sum={sum}></CounterSum> */}
     </div>
   );
 }
